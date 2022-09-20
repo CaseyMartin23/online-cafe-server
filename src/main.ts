6d71eafd19db0fabce153b6ec6b0eba33a9616ea
CaseyMartin23 <cm232311@gmail.com>
2022-09-20 09:16:39 +0200

    added global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || 3223;
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1')
   app.enableCors({ origin: process.env.CLIENT_BASE_URL })
   await app.listen(port);
 }
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, MaxLength } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  price: string;
+
+  @MaxLength(15, {
+    each: true,
+  })
+  categories: string[];
+
+  @MaxLength(5, {
+    each: true,
+  })
+  images: string[];
+
+  @MaxLength(15, {
+    each: true,
+  })
+  tags: string[];
+}
